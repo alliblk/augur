5ffadea155c63dcbaa3480bca0d86607b66bd791
Alli Black <black.alli@gmail.com>
2017-09-21 14:55:14 -0700

    Hard code strain dropping so drop occurs before subsampling.

--- zika/zika.prepare.py
+++ zika/zika.prepare.py
@@ -21,10 +21,13 @@ dropped_strains = [
     "CX17", # large indels in alignment
     "ZF36_36S", # possible contamination
     "Dominican_Republic/2016/PD2", "GD01", "GDZ16001", "VEN/UF_2/2016", # true strains, but duplicates of other strains in dataset
-    "Bahia04", "JAM/2016/WI_JM6", "Bahia11", "Bahia12", "DOM/2016/MA_WGS16_009", "VE_Ganxian", "BRA/2016/FC_DQ60D1", "CX5", # excessive terminal branch length
+    "Bahia04", "JAM/2016/WI_JM6", "Bahia11", "Bahia12", "DOM/2016/MA_WGS16_009", "VE_Ganxian", "BRA/2016/FC_DQ60D1", "CX5", #"Brazil/2016/ZBRY14" # excessive terminal branch length
     "VR10599/Pavia/2016", "34997/Pavia/2016", # exports
     "THA/PLCal_ZV/2013", "SK403/13AS", "SV0010/15", "SK364/13AS", # clock is off
-    "AFMC_U", "AFMC_S" # too basal 
+    "AFMC_U", "AFMC_S", # too basal
+    "USA/2016/FL019", "DOM/2016/MA_WGS16_009", "DOM/2016/MA_WGS16_020", "BRA/2016/FC_DQ12D1", "USA/2016/FL035", "HTI/2016/MA_WGS16_022",
+    "DOM/2016/BB_0428", "HND/2016/HU_ME137", "Bahia04", "Bahia05", "Bahia15", "DOM/2016/MA_WGS16_031", "JAM/2016/MA_WGS16_038", "BRA/2016/FC_DQ68D1",
+    "Brazil/2016/ZBRY12" #sequences have fewer than 5000 informative bases
 ]
 
 config = {
@@ -40,7 +43,7 @@ config = {
         ("Dropped Strains", lambda s: s.id not in [fix_names(x) for x in dropped_strains]),
         ("Restrict Date Range", lambda s: s.attributes['date'] >= datetime(2012,01,1).date()),
         ("Restrict Date Range", lambda s: s.attributes['date'] <= datetime(2018,01,1).date()),
-        ("Sequence Length", lambda s: len(s.seq)>=2000),
+        ("Sequence Length", lambda s: len(s.seq)>=5000),
     ),
     "subsample": {
         "category": lambda x:(x.attributes['date'].year, x.attributes['date'].month, x.attributes['country']),
