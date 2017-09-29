aec7fb22b3dfd3f3ce42e62b5bd99d055f0c3530
Alli Black <black.alli@gmail.com>
2017-09-29 16:34:22 -0700

    update dropped_strains, clock deviations

--- zika/zika.prepare.py
+++ zika/zika.prepare.py
@@ -18,16 +18,55 @@ def collect_args():
     return parser
 
 dropped_strains = [
-    "CX17", # large indels in alignment
-    "ZF36_36S", # possible contamination
-    "Dominican_Republic/2016/PD2", "GD01", "GDZ16001", "VEN/UF_2/2016", # true strains, but duplicates of other strains in dataset
-    "Bahia04", "JAM/2016/WI_JM6", "Bahia11", "Bahia12", "DOM/2016/MA_WGS16_009", "VE_Ganxian", "BRA/2016/FC_DQ60D1", "CX5", #"Brazil/2016/ZBRY14" # excessive terminal branch length
-    "VR10599/Pavia/2016", "34997/Pavia/2016", # exports
-    "THA/PLCal_ZV/2013", "SK403/13AS", "SV0010/15", "SK364/13AS", # clock is off
-    "AFMC_U", "AFMC_S","VIE/Bra/2016", "Brazil/2016/ZBRC28", "Brazil/2015/ZBRC313", # too basal
-    "USA/2016/FL019", "DOM/2016/MA_WGS16_009", "DOM/2016/MA_WGS16_020", "BRA/2016/FC_DQ12D1", "USA/2016/FL035", "HTI/2016/MA_WGS16_022",
-    "DOM/2016/BB_0428", "HND/2016/HU_ME137", "Bahia04", "Bahia05", "Bahia15", "DOM/2016/MA_WGS16_031", "JAM/2016/MA_WGS16_038", "BRA/2016/FC_DQ68D1",
-    "Brazil/2016/ZBRY12" #sequences have fewer than 5000 informative bases
+    # large indels in alignment
+    "CX17",
+    # possible contamination
+    "ZF36_36S",
+    # true strains, but duplicates of other strains in dataset
+    "Dominican_Republic/2016/PD2",
+    "GD01",
+    "GDZ16001",
+    "VEN/UF_2/2016",
+    # excessive terminal branch length
+    "JAM/2016/WI_JM6",
+    "Bahia11",
+    "Bahia12",
+    "DOM/2016/MA_WGS16_009",
+    "VE_Ganxian",
+    "BRA/2016/FC_DQ60D1",
+    "CX5",
+    "Brazil/2016/ZBRY14",
+    # exports
+    "VR10599/Pavia/2016",
+    "34997/Pavia/2016",
+    # clock is off
+    "THA/PLCal_ZV/2013",
+    "SK403/13AS",
+    "SV0010/15",
+    "SK364/13AS",
+    "USA/2016/FLUR057",
+    # too basal
+    "AFMC_U",
+    "AFMC_S",
+    "VIE/Bra/2016",
+    "Brazil/2016/ZBRC28",
+    "Brazil/2015/ZBRC313",
+    #sequences have fewer than 5000 informative bases
+    "USA/2016/FL019",
+    "DOM/2016/MA_WGS16_009",
+    "DOM/2016/MA_WGS16_020",
+    "BRA/2016/FC_DQ12D1",
+    "USA/2016/FL035",
+    "HTI/2016/MA_WGS16_022",
+    "DOM/2016/BB_0428",
+    "HND/2016/HU_ME137",
+    "Bahia04",
+    "Bahia05",
+    "Bahia15",
+    "DOM/2016/MA_WGS16_031",
+    "JAM/2016/MA_WGS16_038",
+    "BRA/2016/FC_DQ68D1",
+    "Brazil/2016/ZBRY12"
 ]
 
 config = {
